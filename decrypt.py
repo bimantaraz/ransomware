4656d39a1f61ce969a9387a14f587609c2c1f9c7
Anugrah Bimantara <90950617+bimantaraz@users.noreply.github.com>
2024-09-07 22:42:25 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,6 @@
 import os
 from cryptography.fernet import Fernet
 
-# Fungsi untuk mendekripsi file
 def dekripsi_file(file_path, key):
     f = Fernet(key)
     with open(file_path, 'rb') as file:
@@ -11,16 +10,10 @@ def dekripsi_file(file_path, key):
         file.write(decrypted_data)
     os.remove(file_path)
 
-# Konfigurasi direktori yang akan didekripsi (desktop)
 DESKTOP_DIR = os.path.join(os.environ.get('USERPROFILE', os.environ.get('HOME', '/')), 'Desktop')
 
-# Kunci enkripsi yang dikirim ke webhook Discord (ganti dengan kunci yang benar)
-key = b'5oo_W9pLi1oSfYIOmRTPj6E26Vs_39pH6BQcx291BCg='  # Sesuaikan dengan kunci yang benar
-
-# Mendekripsi file dengan ekstensi .teraz di direktori desktop
+key = b'KEY_FROM_WEBHOOK'  
 
 for file in os.listdir(DESKTOP_DIR):
     if file.endswith(".teraz"):
         dekripsi_file(os.path.join(DESKTOP_DIR, file), key)
-
-
